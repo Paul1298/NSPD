b7f6ace03249237f027a5f36663e1d8f8dd09388
Khuzhin Pavel <pkh@antrela.ru>
2026-05-26 04:36:32 +0300

    реф

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from pprint import pprint
-
-from pynspd import Nspd, NspdFeature
+from pynspd import Nspd
 
 from data_provider import search_area, process_target, process_neighbors
-
-from geo_processor import get_distance_direction
 from plotting import plot_features
 from report_generator import generate_report
-
 
 
 def main(kad_id, radius_meters=100):
@@ -21,10 +16,11 @@ def main(kad_id, radius_meters=100):
         search_circle_utm = search_area(target, radius_meters)
 
         # 4. Поиск соседних участков
-        processed_neighbors = process_neighbors(target, search_circle_utm, nspd.search_in_contour, crs_4326_to_utm, crs_utm_to_4326)
+        processed_neighbors = process_neighbors(target, search_circle_utm, nspd.search_in_contour, crs_4326_to_utm,
+                                                crs_utm_to_4326)
 
         # Добавляем визуализацию
-        # plot_features(target, processed_neighbors, search_circle_utm, radius_meters)
+        plot_features(target, processed_neighbors, search_circle_utm, radius_meters)
 
         # 6. Генерация отчета
         generate_report(target, processed_neighbors)
