e0e996ee639377e93552b5839c7e19b331061a3b
Khuzhin Pavel <pkh@antrela.ru>
2026-06-16 14:08:39 +0300

    [0.4]added logo.svg

--- nspd_site/urls.py
+++ nspd_site/urls.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
@@ -9,4 +11,7 @@ urlpatterns = [
 # Настройка обслуживания статических файлов графиков
 if settings.DEBUG:
     urlpatterns += static('plots/', document_root=settings.BASE_DIR / 'plots')
+
+if settings.DEBUG or getattr(sys, 'frozen', False):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT or settings.BASE_DIR)
 
